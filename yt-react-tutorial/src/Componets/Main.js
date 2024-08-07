361ebfd5f8a1679461c92bdf55925ee6215b055f
SupunWellappili <supunchandana1814@gmail.com>
2024-08-07 18:48:04 +0530

    image attached another method

--- yt-react-tutorial/src/Componets/Main.js
+++ yt-react-tutorial/src/Componets/Main.js
@@ -1,12 +1,13 @@
 import { Fragment } from "react";
 
 // function Main(props) {
-function Main({ name, city, position }) {
+function Main({ name, city, image, position }) {
   return (
     <div>
       <img
         alt="Laptop picture"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIoXs7lFE--k4gQuVXuK6Re7C7xslvf-NhNg&usqp=CAU"
+        //src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIoXs7lFE--k4gQuVXuK6Re7C7xslvf-NhNg&usqp=CAU"
+        src={image ? `${image}` : `https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRIoXs7lFE--k4gQuVXuK6Re7C7xslvf-NhNg&usqp=CAU`}
       />
       <div className="mainBlock__details">
         <h3>{name}</h3>
