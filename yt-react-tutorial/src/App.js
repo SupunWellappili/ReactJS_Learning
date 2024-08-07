a9dbc34b4eb4c8ab4f9f7b85ec71e84bf004bc04
SupunWellappili <supunchandana1814@gmail.com>
2024-08-07 23:43:03 +0530

    Click Button

--- yt-react-tutorial/src/App.js
+++ yt-react-tutorial/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 import Main from "./Componets/Main";
 import { myData } from "./Data/MyData";
@@ -9,10 +10,18 @@ const mainBlock = myData.map(({ name, city, image, position, id }) => {
 });
 
 function App() {
+
+  let myVar ="Nimal"
+  
+  const clickHandle = () => {
+    myVar = "Pavan";
+    console.log(myVar);
+  };
+
   return (
     <div className="main__container">
       <div>
-        <h1>Supun Wellappili.</h1>
+        <h1>{myVar}</h1>
       </div>
       <p>I'm a Student.</p>
       <div className="mainBlock__container">{mainBlock}</div>
@@ -24,6 +33,8 @@ function App() {
           border: "2px solid red",
           padding: '"7px 12px',
         }}
+        //onClick={clickHandle()} //Not Use
+        onClick={clickHandle}
       >
         Click Me
       </button>
