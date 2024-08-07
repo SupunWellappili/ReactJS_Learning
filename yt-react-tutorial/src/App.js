11b398dd9736556e1180595782b7e403d474d721
SupunWellappili <supunchandana1814@gmail.com>
2024-08-07 22:22:14 +0530

    Inline CSS Added

--- yt-react-tutorial/src/App.js
+++ yt-react-tutorial/src/App.js
@@ -1,20 +1,32 @@
-import { Fragment } from "react";
 import "./App.css";
 import Main from "./Componets/Main";
 import { myData } from "./Data/MyData";
 
-const mainBlock = myData.map(({ name, city,image,position, id }) => {
-  return <Main key={id} name={name} image={image} city={city} position={position} />;
+const mainBlock = myData.map(({ name, city, image, position, id }) => {
+  return (
+    <Main key={id} name={name} image={image} city={city} position={position} />
+  );
 });
 
 function App() {
   return (
     <div className="main__container">
-      <div >
+      <div>
         <h1>Supun Wellappili.</h1>
       </div>
       <p>I'm a Student.</p>
       <div className="mainBlock__container">{mainBlock}</div>
+      <br />
+      <br />
+      <button
+        style={{
+          fontSize: "12px",
+          border: "2px solid red",
+          padding: '"7px 12px',
+        }}
+      >
+        Click Me
+      </button>
     </div>
   );
 }
