112be941854f09cde916b10df6f1fc8458411145
SupunWellappili <supunchandana1814@gmail.com>
2024-08-07 12:47:50 +0530

    Componet arrow function(Capital)

--- yt-react-tutorial/src/App.js
+++ yt-react-tutorial/src/App.js
@@ -2,11 +2,21 @@ import "./App.css";
 import Main from "./Componets/Main";
 import { myData } from "./Data/MyData";
 
-
+/*
 const mainBlock = myData.map(({ name, city, position,id }) => {
   return <Main  key={id} name={name} city={city} position={position} />;
 })
+*/
 
+const NewBlock = ()=>{
+  return (
+    <>
+      {myData.map(({ name, city, position, id }) => {
+        return <Main key={id} name={name} city={city} position={position} />;
+      })}
+    </>
+  );
+}
 
 function App() {
   return (
@@ -23,10 +33,12 @@ function App() {
         myData.map((ele=>{
           return <Main name={ele.name} city={ele.city} position={ele.position}/>
         }))
-
      } */}
+
       {/* //Object Destruction */}
-      {mainBlock}
+      {/* {mainBlock} */}
+
+      <NewBlock />
     </>
   );
 }
