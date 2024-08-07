401a779e0807828610a5f4f3716634b46b2e63ec
SupunWellappili <supunchandana1814@gmail.com>
2024-08-07 11:50:40 +0530

    Export Import Another

--- yt-react-tutorial/src/App.js
+++ yt-react-tutorial/src/App.js
@@ -1,32 +1,6 @@
 import "./App.css";
 import Main from "./Componets/Main";
-
-const myData = [
-  {
-    id : "A-001",
-    name: "Nipuna",
-    city: "Padukka",
-    position: "Electrial",
-  },
-  {
-    id : "A-002",
-    name: "Hasanthi",
-    city: "Buttala",
-    position: "Banker",
-  },
-  {
-    id : "A-003",
-    name: "Malanie",
-    city: "Dickwella",
-    position: "Retire",
-  },
-  {
-    id : "A-004",
-    name: "Wije",
-    city: "Beliatta",
-    position: "GS",
-  },
-];
+import { myData } from "./Data/MyData";
 
 function App() {
   return (
---
+++ yt-react-tutorial/src/Data/MyData.js
@@ -0,0 +1,27 @@
+export const myData = [
+  {
+    id: "A-001",
+    name: "Nipuna",
+    city: "Padukka",
+    position: "Electrial",
+  },
+  {
+    id: "A-002",
+    name: "Hasanthi",
+    city: "Buttala",
+    position: "Banker",
+  },
+  {
+    id: "A-003",
+    name: "Malanie",
+    city: "Dickwella",
+    position: "Retire",
+  },
+  {
+    id: "A-004",
+    name: "Wije",
+    city: "Beliatta",
+    position: "GS",
+  },
+];
+
