4506fb90bf2973caa26e7a813a734a899665aaab
SupunWellappili <supunchandana1814@gmail.com>
2024-08-07 14:58:16 +0530

    ClassName apply

--- yt-react-tutorial/src/App.js
+++ yt-react-tutorial/src/App.js
@@ -3,21 +3,9 @@ import "./App.css";
 import Main from "./Componets/Main";
 import { myData } from "./Data/MyData";
 
-/*
-const mainBlock = myData.map(({ name, city, position,id }) => {
-  return <Main  key={id} name={name} city={city} position={position} />;
-})
-*/
-
-const NewBlock = ()=>{
-  return (
-    <Fragment>
-      {myData?.map(({ name, city, position, id }) => {
-        return <Main key={id} name={name} city={city} position={position} />;
-      })}
-    </Fragment>
-  );
-}
+const mainBlock = myData.map(({ name, city, position, id }) => {
+  return <Main key={id} name={name} city={city} position={position} />;
+});
 
 function App() {
   return (
@@ -25,20 +13,8 @@ function App() {
       <div>
         <h1>Supun Wellappili.</h1>
       </div>
-      <Main>
-        <h5>Supunzzzzz</h5>
-        <p>I'm a children</p>
-      </Main>
-      {/* Object Array (MAP) */}
-      {/* {
-        myData.map((ele=>{
-          return <Main name={ele.name} city={ele.city} position={ele.position}/>
-        }))
-     } */}
-
-      {/* //Object Destruction */}
-      {/* {mainBlock} */}
-      <NewBlock />
+      <p>I'm a Student.</p>
+      <div className="mainBlock_container">{mainBlock}</div>
     </Fragment>
   );
 }
