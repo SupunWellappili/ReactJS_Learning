667646e4bbffc4fe7b927389583b7f6fd4779562
SupunWellappili <supunchandana1814@gmail.com>
2024-08-07 14:12:03 +0530

    Image Added

--- yt-react-tutorial/src/Componets/Main.js
+++ yt-react-tutorial/src/Componets/Main.js
@@ -1,39 +1,22 @@
-/*
-const name = "Supun Wellapili";
-const position = "Web Developer";
-
-const myObj = {
-  // create object
-  name: `${name}`,
-  position: `${position}`,
-  city: "Galle",
-};
-*/
-
+import { Fragment } from "react";
 
 // function Main(props) {
-function Main({ children, name,city, position }) {
-  //   console.log(props);
-
-  //const { city } = myObj; //Object Destructing
-
+function Main({ name, city, position }) {
   return (
-    <div>
-      {/* This is main component */}
-      {/* <ul>
-        <li>Java</li>
-        <li>C+</li>
-        <li>C#</li>
-        <li>Python</li>
-      </ul> */}
-      <div>{name}</div>
-      {/* <div>{myObj.position}</div> */}
-      <div>{position}</div>
-      <div>{city}</div>
-      <div>--------------------------------------------</div>
-      {/* {props.children} */}
-      {/* {children} */}
-    </div>
+    <Fragment>
+        <div>--------------------------------------------</div>
+      <img
+        alt="Laptop picture"
+        src="https://ssl.robocup.org/wp-content/uploads/2021/12/20160629-IMG_0283_1.jpg"
+      />
+      <div>
+        <h3>{name}</h3>
+        <p>
+          <span>{city}</span>
+          <span>{position}</span>
+        </p>
+      </div>
+    </Fragment>
   );
 }
 
